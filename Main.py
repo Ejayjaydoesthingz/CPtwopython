5178b71f838ee487de111130aa3c75231781680e
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2026-02-23 15:55:26 +0800

    Python first lesson exercises

---
+++ Main.py
@@ -0,0 +1,62 @@
+"""item = "potato chips"
+price = 35.45
+quantity = 100
+isFood = True
+"""
+#print(type())
+
+"""numberStr = "10.40"
+number = 10.25
+numberStr = float(numberStr)
+
+total = numberStr + number
+
+print (type(numberStr))
+print(total)"""
+
+"""name = "This is my name"
+name = bool(name)
+
+print(type(name))"""
+
+#print("enter your first name:")
+"""Firstname = input("enter your first name: ") 
+print(f"Hi {Firstname}!")"""
+
+"""numberOne = input("enter a value of num one: ")
+numberTwo = input("enter a value of num two: ")"""
+
+"""numberOne = int(numberOne)
+numberTwo = int(numberTwo)
+sum = numberOne + numberTwo
+print(f"the sum of two numbers is {sum}")"""
+
+#Exercise 1
+"""W = int(input("enter your width: "))
+L = int(input("enter your length: "))
+
+A = W * L
+
+print(f"A = {A}")"""
+
+#Exercise 2
+"""print(input("what item would you like to buy?"))
+
+price = float(input("what's the price? "))
+quantity = int(input("how many would you like to buy? "))
+
+total = price * quantity
+
+print(f"Your total would be {total}")"""
+
+age = int(input("Enter ur age: "))
+
+if age <= 5:
+    print("why r u on the puter little bro")
+elif age >= 13 and age <= 19:
+    print("teenage dirtbag?")
+else:
+    print("what is up unc")
+    
+    
+
