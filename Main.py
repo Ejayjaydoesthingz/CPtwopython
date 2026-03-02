3c6e1d63920acd4e77d4f5fa8d60c73b73203de5
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2026-03-02 15:57:44 +0800

    python lesson 2

--- Main.py
+++ Main.py
@@ -49,14 +49,66 @@ total = price * quantity
 
 print(f"Your total would be {total}")"""
 
-age = int(input("Enter ur age: "))
+"""age = int(input("Enter ur age: "))
 
 if age <= 5:
     print("why r u on the puter little bro")
 elif age >= 13 and age <= 19:
     print("teenage dirtbag?")
 else:
-    print("what is up unc")
+    print("what is up unc")"""
     
+#example wan
+
+"""number = 7
+
+result = "postive" if number > 0 else "negative"
+
+print(f"the result is {result}")"""
+
+#example two
+
+"""min = int(input("enter a minimum value: "))
+max = int(input("enter a maximum value: "))
+
+result = min if min < max else max 
+print(f"the result is {result}")"""
+
+#example 3
+
+"""text = input("enter a text: ")"""
+
+"""print(f"the length of the text is {len(text)}")"""
+
+"""result = text.find("p")
+print(result)"""
+"""resultOne = text.capitalize()
+resultTwo = resultOne.upper()
+resultThree = resultTwo.lower()
+print(f"Capitalize: {resultOne}, Upper: {resultTwo}, Lower: {resultThree}")"""
+"""result = text.isdigit()"""
+#result = text.isalpha()
+"""result = text.count("T")
+print(result)"""
+
+#exercise wan
+
+"""username = input("please enter username: ")
+
+if " " in username:
+    print("Error: Username must not contain spaces.")
+elif len(username) > 12:
+    print("Your username must not go beyond 12 characters.")
+elif not username.isalpha():
+    print("your user must not contain a digit.")
+else:
+    print(f"welcome {username}!!")"""
+    
+credit_card = "1267-2546-7674-9672"
+last_four = credit_card[-4:]
+print("last four digits:", last_four)
+
+reversed_card = credit_card[::-1]
+print("reversed:", reversed_card)
     
 
